11608f5174ac4750e5690899df2991d8b644ef5d
gonzagaa <iigust4vo@gmail.com>
2024-07-12 18:09:09 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,102 @@
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]");
+
+window.addEventListener('mousemove', function (e) {
+
+  const posX = e.clientX;
+  const posY = e.clientY;
+
+  cursorDot.style.left = `${posX}px`
+  cursorDot.style.top = `${posY}px`
+
+  cursorOutline.style.left = `${posX}px`
+  cursorOutline.style.top = `${posY}px`
+
+  cursorOutline.animate({
+    left: `${posX}px`,
+    top: `${posY}px`
+  }, {duration: 500, fill: "forwards"});
+})
+
+
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+function onScroll() {
+    showNavOnScroll()
+}
+
+function showNavOnScroll() {
+    if(scrollY > 0) {
+        document.querySelector("#navigation").classList.add("scroll")
+    } else {
+        document.querySelector("#navigation").classList.remove("scroll")
+    }
+}
+
+function openMenu() {
+    document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+    document.body.classList.remove('menu-expanded')
+}
+
+ //smoothscroll
+//  const body = document.body,
+//  scrollWrap = document.getElementsByClassName("smooth-scroll-wrapper")[0],
+//  height = scrollWrap.getBoundingClientRect().height - 1,
+//  speed = 0.04;
+
+//  var offset = 0;
+
+//  body.style.height = Math.floor(height) + "px";
+
+//  function smoothScroll() {
+//  offset += (window.pageYOffset - offset) * speed;
+
+//  var scroll = "translateY(-" + offset + "px) translateZ(0)";
+//  scrollWrap.style.transform = scroll;
+
+//  callScroll = requestAnimationFrame(smoothScroll);
+// }
+
+//  smoothScroll();
+
+
+
+
+const larguraDaTela = window.innerWidth
+  
+if (larguraDaTela < 800) {
+    var swiper3 = new Swiper(".mySwiper3", {
+        grabCursor: true,
+        effect: "creative",
+        autoplay: {
+            delay: 3500,
+            disableOnInteraction: false,
+          },
+        creativeEffect: {
+          prev: {
+            shadow: true,
+            translate: ["-20%", 0, -1],
+          },
+          next: {
+            translate: ["100%", 0, 0],
+          },
+        },
+      });
+} else {
+    var swiper3 = new Swiper(".mySwiper3", {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        loop: true,
+        grabCursor: true,
+        freeMode: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+}
+  
