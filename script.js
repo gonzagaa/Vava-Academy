fc697e3bd006b8a948b9604b742a9b706566fe2d
gonzagaa <iigust4vo@gmail.com>
2024-07-20 12:35:03 -0300

    update

--- script.js
+++ script.js
@@ -34,25 +34,25 @@ function closeMenu() {
 }
 
  //smoothscroll
-//  const body = document.body,
-//   scrollWrap = document.getElementsByClassName("smooth-scroll-wrapper")[0],
-//   height = scrollWrap.getBoundingClientRect().height - 1,
-//   speed = 0.04;
+//   const bodyy = document.body,
+//    scrollWrap = document.getElementsByClassName("smooth-scroll-wrapper")[0],
+//    height = scrollWrap.getBoundingClientRect().height - 1,
+//    speed = 0.04;
 
-//   var offset = 0;
+// var offset = 0;
 
-//   body.style.height = Math.floor(height) + "px";
+//    bodyy.style.height = Math.floor(height) + "px";
 
-//   function smoothScroll() {
-//  offset += (window.pageYOffset - offset) * speed;
+//    function smoothScroll() {
+//   offset += (window.pageYOffset - offset) * speed;
 
-//  var scroll = "translateY(-" + offset + "px) translateZ(0)";
+//   var scroll = "translateY(-" + offset + "px) translateZ(0)";
 // scrollWrap.style.transform = scroll;
 
-//  callScroll = requestAnimationFrame(smoothScroll);
-// }
+//   callScroll = requestAnimationFrame(smoothScroll);
+//  }
 
-//  smoothScroll();
+//   smoothScroll();
 
 
 
